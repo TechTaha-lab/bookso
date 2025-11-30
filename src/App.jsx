2730cab4957bc4b9da3ae337bc730f12bd8bfe2f
TechTaha-lab <92130352@students.liu.edu.lb>
2025-11-30 13:30:57 +0000

    sms

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/user/Home";
 import About from "./components/user/About";
